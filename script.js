89626e723491b6bac339deb44b04f9c305e020ba
Masha Martyniuk <mmartyniuk004@gmail.com>
2023-05-17 10:07:02 +0300

    added the display of the typed text
    + the typed text changes colour to a blank one. Functionality was implemented for both variants (string of letters and text).

--- script.js
+++ script.js
@@ -1,58 +1,57 @@
-const input = document.querySelector("input");
-const letters = Array.from(document.querySelectorAll("[data-letters]"));
-const specs = Array.from(document.querySelectorAll("[data-spec]"));
+const input = document.querySelector('input');
+const letters = Array.from(document.querySelectorAll('[data-letters]'));
+const specs = Array.from(document.querySelectorAll('[data-spec]'));
+const textExample = document.querySelector('#textExample');
 
-const text = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-mollit anim id est laborum...`;
+const text = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in...`;
 
 const party = createParty(text);
 console.log(party);
 init();
 
 function init() {
-  input.addEventListener("keydown", keydownHandler);
-  input.addEventListener("keyup", keyupHandler);
+  input.addEventListener('keydown', keyDownHandler);
+  input.addEventListener('keyup', keyUpHandler);
 }
 function toggleKeyClass(key, add) {
   const matchingKeys = letters.filter((x) => x.dataset.letters.includes(key));
 
-  matchingKeys.forEach((key) => key.classList.toggle("pressed", add));
+  matchingKeys.forEach((key) => key.classList.toggle('pressed', add));
 
   const ownSpec = specs.filter((x) => x.dataset.spec === key);
 
   if (ownSpec.length) {
-    ownSpec.forEach((spec) => spec.classList.toggle("pressed", add));
+    ownSpec.forEach((spec) => spec.classList.toggle('pressed', add));
   }
 }
 
-function keydownHandler(event) {
+function keyDownHandler(event) {
   event.preventDefault();
 
-  const isSpaceKey = event.key === " ";
-  const pressedKey = isSpaceKey ? "space" : event.key.toLowerCase();
+  const isSpaceKey = event.key === ' ';
+  const pressedKey = isSpaceKey ? 'space' : event.key.toLowerCase();
 
   toggleKeyClass(pressedKey, true);
 
-  if (pressedKey === "shift") {
-    toggleKeyClass("shift", true);
+  if (pressedKey === 'shift') {
+    toggleKeyClass('shift', true);
   }
+  if (isSpaceKey) press(' ');
+  if (pressedKey === 'enter') press('\n');
+  press(event.key);
 }
 
-function keyupHandler(event) {
+function keyUpHandler(event) {
   event.preventDefault();
 
-  const isSpaceKey = event.key === " ";
-  const pressedKey = isSpaceKey ? "space" : event.key.toLowerCase();
+  const isSpaceKey = event.key === ' ';
+  const pressedKey = isSpaceKey ? 'space' : event.key.toLowerCase();
 
   toggleKeyClass(pressedKey, false);
 
-  if (pressedKey === "shift") {
-    toggleKeyClass("shift", false);
+  if (pressedKey === 'shift') {
+    toggleKeyClass('shift', false);
   }
 }
 
@@ -61,32 +60,85 @@ function createParty(text) {
     text,
     strings: [],
     maxStringLength: 70,
-    maxShowStrings: 3,
+    maxShowStrings: 5,
     currentStringIndex: 0,
-    currentPrintedIndex: 0,
+    currentPressedIndex: 0,
     errors: [],
   };
-  party.text = party.text.replace(/\n/g, "\n ");
-  const words = party.text.split(" ");
+  party.text = party.text.replace(/\n/g, '\n ');
+  const words = party.text.split(' ');
   let string = [];
 
   for (const word of words) {
-    const newStringLength =
-      [...string, word].join(" ").length + !word.includes("\n");
+    const newStringLength = [...string, word].join(' ').length + !word.includes('\n');
 
     if (newStringLength > party.maxStringLength) {
-      party.strings.push(string.join(" ") + " ");
+      party.strings.push(string.join(' ') + ' ');
       string = [];
     }
 
     string.push(word);
 
-    if (word.includes("\n")) {
-      party.strings.push(string.join(" "));
+    if (word.includes('\n')) {
+      party.strings.push(string.join(' '));
     }
   }
   if (string.length) {
-    party.strings.push(string.join(" "));
+    party.strings.push(string.join(' '));
   }
   return party;
+}
+
+function press(pressedKey) {
+  const string = party.strings[party.currentStringIndex];
+  const mustKey = string[party.currentPressedIndex];
+  console.log(pressedKey, mustKey);
+  if (pressedKey === mustKey) {
+    party.currentPressedIndex++;
+    if (string.length <= party.currentPressedIndex) {
+      party.currentPressedIndex = 0;
+      party.currentStringIndex++;
+    }
+  } else if (!party.errors.includes(mustKey)) party.errors.push(mustKey);
+  viewUpdate();
+}
+
+function viewUpdate() {
+  const string = party.strings[party.currentStringIndex];
+  const shownString = party.strings.slice(
+    party.currentStringIndex,
+    party.currentStringIndex + party.maxShowStrings
+  );
+  const div = document.createElement('div');
+  const firstLine = document.createElement('div');
+  firstLine.classList.add('line');
+  div.append(firstLine);
+  const doneLine = document.createElement('span');
+  doneLine.classList.add('done');
+  doneLine.textContent = string.slice(0, party.currentPressedIndex);
+
+  firstLine.append(
+    doneLine,
+    ...string
+      .slice(party.currentPressedIndex)
+      .split('')
+      .map((pressedKey) => {
+        return pressedKey;
+      })
+  );
+
+  for (let i = 2; i < shownString.length; i++) {
+    const line = document.createElement('div');
+    line.classList.add('line');
+    div.append(line);
+
+    line.append(
+      ...shownString[i].split('').map((pressedKey) => {
+        return pressedKey;
+      })
+    );
+  }
+  textExample.innerHTML = '';
+  textExample.append(div);
+  input.value = string.slice(0, party.currentPressedIndex);
 }
--- scriptEasy.js
+++ scriptEasy.js
@@ -1,58 +1,56 @@
-const input = document.querySelector("input");
-const letters = Array.from(document.querySelectorAll("[data-letters]"));
-const specs = Array.from(document.querySelectorAll("[data-spec]"));
+const input = document.querySelector('input');
+const letters = Array.from(document.querySelectorAll('[data-letters]'));
+const specs = Array.from(document.querySelectorAll('[data-spec]'));
+const textExample = document.querySelector('#textExample');
 
-const text = `ass anv anb dd ssa ert lor s-- abc abc fde qwe
-asw wasd wsad uy yy yyy pps ';s we q we ali d adf ety vbv zzx 
-doop ffdr rru n dde kopp kdowwa kksk ert d d d aas ll kjnc ccj
-eoeoe ith as its yor sd plp a zxc fgo daw qw a;okf vvb dfd ,.. f
-fieeh ty alpoi ertp wetkc a[oj] cnvd owasd vnx`;
+const text = `ass anv anb dd ssa ert lor s-- abc abc fde qwe asw wasd wsad`;
 
 const party = createParty(text);
-console.log(party);
 init();
 
 function init() {
-  input.addEventListener("keydown", keyDownHandler);
-  input.addEventListener("keyup", keyUpHandler);
+  input.addEventListener('keydown', keyDownHandler);
+  input.addEventListener('keyup', keyUpHandler);
+  viewUpdate();
 }
 function toggleKeyClass(key, add) {
   const matchingKeys = letters.filter((x) => x.dataset.letters.includes(key));
 
-  matchingKeys.forEach((key) => key.classList.toggle("pressed", add));
+  matchingKeys.forEach((key) => key.classList.toggle('pressed', add));
 
   const ownSpec = specs.filter((x) => x.dataset.spec === key);
 
   if (ownSpec.length) {
-    ownSpec.forEach((spec) => spec.classList.toggle("pressed", add));
+    ownSpec.forEach((spec) => spec.classList.toggle('pressed', add));
   }
 }
 
 function keyDownHandler(event) {
   event.preventDefault();
 
-  const isSpaceKey = event.key === " ";
-
-  const pressedKey = isSpaceKey ? "space" : event.key.toLowerCase();
+  const isSpaceKey = event.key === ' ';
+  const pressedKey = isSpaceKey ? 'space' : event.key.toLowerCase();
 
   toggleKeyClass(pressedKey, true);
 
-  if (pressedKey === "shift") {
-    toggleKeyClass("shift", true);
+  if (pressedKey === 'shift') {
+    toggleKeyClass('shift', true);
   }
+  if (isSpaceKey) press(' ');
+  if (pressedKey === 'enter') press('\n');
+  press(event.key);
 }
 
 function keyUpHandler(event) {
   event.preventDefault();
 
-  const isSpaceKey = event.key === " ";
-
-  const pressedKey = isSpaceKey ? "space" : event.key.toLowerCase();
+  const isSpaceKey = event.key === ' ';
+  const pressedKey = isSpaceKey ? 'space' : event.key.toLowerCase();
 
   toggleKeyClass(pressedKey, false);
 
-  if (pressedKey === "shift") {
-    toggleKeyClass("shift", false);
+  if (pressedKey === 'shift') {
+    toggleKeyClass('shift', false);
   }
 }
 
@@ -63,30 +61,82 @@ function createParty(text) {
     maxStringLength: 70,
     maxShowStrings: 3,
     currentStringIndex: 0,
-    currentPrintedIndex: 0,
+    currentPressedIndex: 0,
     errors: [],
   };
-  party.text = party.text.replace(/\n/g, "\n ");
-  const words = party.text.split(" ");
+  party.text = party.text.replace(/\n/g, '\n ');
+  const words = party.text.split(' ');
   let string = [];
 
   for (const word of words) {
-    const newStringLength =
-      [...string, word].join(" ").length + !word.includes("\n");
+    const newStringLength = [...string, word].join(' ').length + !word.includes('\n');
 
     if (newStringLength > party.maxStringLength) {
-      party.strings.push(string.join(" ") + " ");
+      party.strings.push(string.join(' ') + ' ');
       string = [];
     }
 
     string.push(word);
 
-    if (word.includes("\n")) {
-      party.strings.push(string.join(" "));
+    if (word.includes('\n')) {
+      party.strings.push(string.join(' '));
     }
   }
   if (string.length) {
-    party.strings.push(string.join(" "));
+    party.strings.push(string.join(' '));
   }
   return party;
+}
+
+function press(pressedKey) {
+  const string = party.strings[party.currentStringIndex];
+  const mustKey = string[party.currentPressedIndex];
+  if (pressedKey === mustKey) {
+    party.currentPressedIndex++;
+    if (string.length < party.currentPressedIndex) {
+      party.currentPressedIndex = 0;
+      party.currentStringIndex++;
+    }
+  } else if (!party.errors.includes(mustKey)) party.errors.push(mustKey);
+  viewUpdate();
+}
+
+function viewUpdate() {
+  const string = party.strings[party.currentStringIndex];
+  const shownString = party.strings.slice(
+    party.currentStringIndex,
+    party.currentStringIndex + party.maxShowStrings
+  );
+  const div = document.createElement('div');
+  const firstLine = document.createElement('div');
+  firstLine.classList.add('line');
+  div.append(firstLine);
+  const doneLine = document.createElement('span');
+  doneLine.classList.add('done');
+  doneLine.textContent = string.slice(0, party.currentPressedIndex);
+
+  firstLine.append(
+    doneLine,
+    ...string
+      .slice(party.currentPressedIndex)
+      .split('')
+      .map((pressedKey) => {
+        return pressedKey;
+      })
+  );
+
+  for (let i = 1; i < shownString.length; i++) {
+    const line = document.createElement('div');
+    line.classList.add('line');
+    div.append(line);
+
+    line.append(
+      ...shownString[i].split('').map((pressedKey) => {
+        return pressedKey;
+      })
+    );
+  }
+  textExample.innerHTML = '';
+  textExample.append(div);
+  input.value = string.slice(0, party.currentPressedIndex);
 }
