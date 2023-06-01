dde4e1fe190266c6f7e0087309c9a2b8e40942d5
Masha Martyniuk <mmartyniuk004@gmail.com>
2023-06-01 12:49:01 +0300

    implemented registration with password encription
    For password encription I used the bcrypt function. Also I added the data validation, so the entered username/password cannot be empty + password shouldn't be shorter than 8 symbols.

--- controller.js
+++ controller.js
@@ -1,24 +1,44 @@
 const { json } = require('express');
 const User = require('./user');
 const Role = require('./roles');
+const bcrypt = require('bcryptjs');
+const { validationResult } = require('express-validator');
 
 class Сontroller {
   async registration(req, res) {
     try {
-    } catch (e) {}
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ message: `Error during resgistration`, errors });
+      }
+      const { username, password } = req.body;
+      const candidate = await User.findOne({ username });
+      if (candidate) {
+        return res.status(400).json({ message: `User with such name already exists` });
+      }
+      const hashPassword = bcrypt.hashSync(password, 8);
+      const userRole = await Role.findOne({ value: 'USER' });
+      const user = new User({ username, password: hashPassword, roles: [userRole.value] });
+      await user.save();
+      return res.json({ message: `User was successfully created!` });
+    } catch (e) {
+      console.log(e);
+      res.status(400).json({ message: `Registration error` });
+    }
   }
   async login(req, res) {
     try {
-    } catch (e) {}
+    } catch (e) {
+      console.log(e);
+      res.status(400).json({ message: `Login error` });
+    }
   }
   async getUsers(req, res) {
     try {
-      const userRole = new Role();
-      const adminRole = new Role({ value: 'ADMIN' });
-      await userRole.save();
-      await adminRole.save();
       res.json('server works');
-    } catch (e) {}
+    } catch (e) {
+      console.log(e);
+    }
   }
 }
 
--- routers.js
+++ routers.js
@@ -1,8 +1,16 @@
 const Router = require('express');
 const router = new Router();
 const controller = require('./controller');
+const { check } = require('express-validator');
 
-router.post('/registration', controller.registration);
+router.post(
+  '/registration',
+  [
+    check('username', 'Username cannot be empty.').notEmpty(),
+    check('password', 'Password should not be shorter than 8 symbols.').isLength({ min: 8 }),
+  ],
+  controller.registration
+);
 router.post('/login', controller.login);
 router.get('/users', controller.getUsers);
 
