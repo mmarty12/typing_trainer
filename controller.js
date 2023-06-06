a63a7d66d1ace3a072595b33faca8fb24e68a18d
Masha Martyniuk <mmartyniuk004@gmail.com>
2023-06-06 17:46:10 +0300

    updated some message output

--- controller.js
+++ controller.js
@@ -3,7 +3,7 @@ const User = require('./user');
 const Role = require('./roles');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const {secret} = require('./config');
+const { secret } = require('./config');
 const { validationResult } = require('express-validator');
 
 const generateAccessToken = (id, roles) => {
@@ -12,14 +12,14 @@ const generateAccessToken = (id, roles) => {
     roles,
   };
   return jwt.sign(payload, secret, { expiresIn: '24h' });
-}
+};
 
 class Сontroller {
   async registration(req, res) {
     try {
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
-        return res.status(400).json({ message: `Error during resgistration`, errors });
+        return res.status(400).json({ message: `Registration error`, errors });
       }
       const { username, password } = req.body;
       const candidate = await User.findOne({ username });
@@ -38,18 +38,18 @@ class Сontroller {
   }
   async login(req, res) {
     try {
-      const {username, password} = req.body;
-      const user = await User.findOne({username});
+      const { username, password } = req.body;
+      const user = await User.findOne({ username });
       if (!user) {
         return res.status(400).json({ message: `User with such name does not exist` });
       }
       const validPassword = bcrypt.compareSync(password, user.password);
-    if (!validPassword) {
-      return res.status(400).json({ message: `Invalid password` })
-    } 
-    const token = generateAccessToken(user._id, user.roles);
-    return res.json({ token });  
-  } catch (e) {
+      if (!validPassword) {
+        return res.status(400).json({ message: `Invalid password` });
+      }
+      const token = generateAccessToken(user._id, user.roles);
+      return res.json({ token });
+    } catch (e) {
       console.log(e);
       res.status(400).json({ message: `Login error` });
     }
